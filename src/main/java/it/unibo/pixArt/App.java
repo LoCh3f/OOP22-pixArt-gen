7b381dda2bf0d35a023f75a66815c699922bde8b
Aldo Visconti <aldovisco20000@gmail.com>
2023-03-27 17:36:58 +0200

    Model Implementation

--- src/main/java/it/unibo/pixArt/App.java
+++ src/main/java/it/unibo/pixArt/App.java
@@ -1,20 +1,20 @@
-package  it.unibo.pixArt;
+package it.unibo.pixArt;
 
-import it.unibo.pixArt.model.Model;
+import it.unibo.pixArt.model.ModelImpl;
 import it.unibo.pixArt.view.pages.PageLoader;
 import it.unibo.pixArt.view.pages.Pages;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-public class App extends  Application {
+public class App extends Application {
 
 
-    public static  void main(final String[] args) {
+    public static void main(final String[] args) {
         launch(args);
     }
 
     @Override
     public void start(final Stage primaryStage) {
-        PageLoader.getInstance().switchPage(primaryStage, Pages.MENU, new Abstr );
+        PageLoader.getInstance().switchPage(primaryStage, Pages.MENU, new ModelImpl());
     }
 }
--- src/main/java/it/unibo/pixArt/model/ModelImpl.java
+++ src/main/java/it/unibo/pixArt/model/ModelImpl.java
@@ -3,12 +3,15 @@ package it.unibo.pixArt.model;
 import it.unibo.pixArt.model.api.User;
 import it.unibo.pixArt.model.impl.UserImpl;
 
-public abstract class AbstractModel implements Model {
+public class ModelImpl implements Model {
 
     private User user;
 
-    public AbstractModel(final String name, final String path) {
+    public ModelImpl(final String name, final String path) {
         this.user = new UserImpl(name, path);
+    }
+
+    public ModelImpl() {
     }
 
     @Override
@@ -20,4 +23,5 @@ public abstract class AbstractModel implements Model {
     public void setUser(final String name, final String path) {
         this.user = new UserImpl(name, path);
     }
+
 }
