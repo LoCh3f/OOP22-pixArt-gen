db2261e7b02aed40f82776cbbc598dbbee1fd610
Aldo Visconti <aldovisco20000@gmail.com>
2023-04-23 03:05:36 +0200

    PixelMatrix->MatrixBuilder.java basic implementation

---
+++ src/main/java/it/unibo/pixArt/model/grid/PixelMatrix.java
@@ -0,0 +1,59 @@
+package it.unibo.pixArt.model.grid;
+
+import it.unibo.pixArt.model.pixel.ImplPixel;
+import it.unibo.pixArt.model.pixel.Pixel;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.function.Consumer;
+
+public class PixelMatrix {
+
+    private final int rows;
+
+    private final int columns;
+
+    private final Set<Pixel> pixels;
+
+    private PixelMatrix(final int rows, final int columns) {
+        this.rows = rows;
+        this.columns = columns;
+        this.pixels = new HashSet<>(rows * columns);
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                pixels.add(new ImplPixel(i, j));
+            }
+        }
+    }
+
+    public static class MatrixBuilder {
+
+        private int rows;
+        private int columns;
+
+        public MatrixBuilder() {
+            this.rows = 0;
+            this.columns = 0;
+        }
+
+        public MatrixBuilder setRows(final int rows) {
+            this.rows = rows;
+            return this;
+        }
+
+        public MatrixBuilder setColumns(final int columns) {
+            this.columns = columns;
+            return this;
+        }
+
+        public PixelMatrix build() throws  IllegalStateException {
+
+            return new PixelMatrix(this.rows, this.columns);
+        }
+
+    }
+
+    public void update(final Consumer<Pixel> consumer, Pixel pixel) {
+        this.pixels.forEach(consumer);
+    }
+}
