34bd00017d53b292fadb696f4ee5c6392026080b
Aldo Visconti <aldovisco20000@gmail.com>
2023-04-30 14:57:31 +0200

    GridPane setted for WorkSpace view

--- src/main/java/it/unibo/pixArt/view/workspace/WorkSpace.java
+++ src/main/java/it/unibo/pixArt/view/workspace/WorkSpace.java
@@ -1,18 +1,36 @@
 package it.unibo.pixArt.view.workspace;
 
 import it.unibo.pixArt.view.AbstractFXView;
+import javafx.fxml.FXML;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 
 public class WorkSpace extends AbstractFXView {
 
+    @FXML
+    public MenuBar menubar;
+    @FXML
+    private BorderPane root;
+    @FXML
+    private ColorPicker colorPicker;
+    @FXML
+    private ScrollPane frames;
     private GridPane center;
 
 
     @Override
     public void init() {
-        final var root = (BorderPane) this.getStage().getScene().getRoot();
-        this.center = new GridPane();
+        final var rows = this.getController().getModel().getProject().getAllFrames().get(0).getRows();
+        final var columns = this.getController().getModel().getProject().getAllFrames().get(0).getColumns();
+        this.center = new CenterPane.GridPaneBuilder()
+                .setColumns(columns).setRows(rows)
+                .setGridLinesVisible(true)
+                .setHeight(root.heightProperty().subtract(frames.heightProperty().add(menubar.heightProperty())))
+                .setHeight(root.widthProperty().subtract(colorPicker.widthProperty())).build().get();
+        this.root.setCenter(center);
 
     }
 }
