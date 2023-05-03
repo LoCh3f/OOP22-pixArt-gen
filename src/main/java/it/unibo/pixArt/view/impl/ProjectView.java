7398e4dc9d21982c2b35d9f833b30b38088b1904
Alessandro Ricci <alessandro.ricci38@studio.unibo.it>
2023-05-03 16:44:10 +0200

    modified changeDirectory method in ProjectView

--- src/main/java/it/unibo/pixArt/view/impl/ProjectView.java
+++ src/main/java/it/unibo/pixArt/view/impl/ProjectView.java
@@ -7,6 +7,7 @@ import java.util.stream.Stream;
 
 import javax.swing.Action;
 
+import it.unibo.pixArt.model.user.UserImpl;
 import it.unibo.pixArt.view.AbstractFXView;
 import it.unibo.pixArt.view.SimpleView;
 import it.unibo.pixArt.view.pages.PageLoader;
@@ -14,18 +15,21 @@ import it.unibo.pixArt.view.pages.Pages;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.Stage;
 
 public class ProjectView extends AbstractFXView {
 
     @FXML
-    ListView<String> listView = new ListView<>();
+    private ListView<String> listView = new ListView<>();
+    private UserImpl user = new UserImpl(null, System.getProperty("user.dir"));
 
     @Override
     public void init() {
         // TODO Auto-generated method stub
         //throw new UnsupportedOperationException("Unimplemented method 'init'");
 
-        listView.getItems().addAll(Stream.of((new File(System.getProperty("user.dir")).listFiles()))
+        listView.getItems().addAll(Stream.of((new File(user.getPathToFile()).listFiles()))
                                   .filter(file -> file.isDirectory() && !file.isHidden())
                                   .map(File::getName).collect(Collectors.toCollection(ArrayList::new)));
     }
@@ -43,7 +47,13 @@ public class ProjectView extends AbstractFXView {
     }
   
     public void onChangeDirClick(ActionEvent event){
-        
+        Stage primaryStage = new Stage();
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        directoryChooser.setInitialDirectory(new File(user.getPathToFile()));    
+        File selectedDirectory = directoryChooser.showDialog(primaryStage);
+        user.setPathToFile(selectedDirectory.getAbsolutePath());
+        listView.getItems().clear();
+        init();        
     }
 
 }
