b561c1b18cb8edc79e0521a89292f78459ae6cff
Eduard Toni Alexandru <eduardtoni.alexandru@studio.unibo.it>
2023-05-03 17:22:11 +0200

    Added FrameState constructor.

--- src/main/java/it/unibo/pixArt/model/framestate/FrameStateImpl.java
+++ src/main/java/it/unibo/pixArt/model/framestate/FrameStateImpl.java
@@ -9,6 +9,10 @@ import it.unibo.pixArt.model.pixel.Pixel;
 public class FrameStateImpl implements FrameState {
     private Stack<Set<Pixel>> stateStack = new Stack<>();
 
+    public FrameStateImpl() {
+
+    }
+    
     @Override
     public void setState(final Set<Pixel> snapshot) {
        this.stateStack.push(snapshot);
