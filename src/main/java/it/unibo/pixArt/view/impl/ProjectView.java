75a9f8d35f27a0efc11b0481118395ae79d6b0b4
Alessandro Ricci <alessandro.ricci38@studio.unibo.it>
2023-05-03 14:38:06 +0200

    modified init method in ProjectView and Project.fxml

--- src/main/java/it/unibo/pixArt/view/impl/ProjectView.java
+++ src/main/java/it/unibo/pixArt/view/impl/ProjectView.java
@@ -1,5 +1,10 @@
 package it.unibo.pixArt.view.impl;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import javax.swing.Action;
 
 import it.unibo.pixArt.view.AbstractFXView;
@@ -8,13 +13,21 @@ import it.unibo.pixArt.view.pages.PageLoader;
 import it.unibo.pixArt.view.pages.Pages;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
 
 public class ProjectView extends AbstractFXView {
+
+    @FXML
+    ListView<String> listView = new ListView<>();
 
     @Override
     public void init() {
         // TODO Auto-generated method stub
         //throw new UnsupportedOperationException("Unimplemented method 'init'");
+
+        listView.getItems().addAll(Stream.of((new File(System.getProperty("user.dir")).listFiles()))
+                                  .filter(file -> file.isDirectory() && !file.isHidden())
+                                  .map(File::getName).collect(Collectors.toCollection(ArrayList::new)));
     }
 
     public void onHomeClick(final ActionEvent event){
