b9e290ffe82b35e8386282f5adef7c1b15c09b72
Aldo Visconti <aldovisco20000@gmail.com>
2023-05-08 17:13:28 +0200

    new utility class implements Function<T,R>, PixelsParser.java & GridPaneParser.java

---
+++ src/main/java/it/unibo/pixArt/utilities/PixelsParser.java
@@ -0,0 +1,27 @@
+package it.unibo.pixArt.utilities;
+
+import it.unibo.pixArt.model.pixel.Pixel;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+
+import java.util.Collection;
+import java.util.function.Function;
+
+import static it.unibo.pixArt.utilities.FXStyleVariable.*;
+
+public class PixelsParser implements Function<Collection<Pixel>, GridPane> {
+    @Override
+    public GridPane apply(Collection<Pixel> pixels) {
+        final var gridPane = new GridPane();
+        for (Pixel p : pixels) {
+            gridPane.add(new Button(), p.getPosition().getX(), p.getPosition().getY());
+            gridPane.getChildren().get(gridPane.getChildren().size() - 1).setStyle(parseColor(p.getColor()));
+        }
+        return gridPane;
+    }
+
+    private String parseColor(final Color color) {
+        return "".concat(FX_BACKGROUND_COLOR_START).concat(color.toString()).concat(";").concat(FX_BORDER_COLOR).concat(";").concat(FX_BORDER_WIDTH);
+    }
+}
