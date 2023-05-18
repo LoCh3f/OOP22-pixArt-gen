ad0fdf52c0152714abc85291a7a8b136d0a7a79c
Eduard Toni Alexandru <eduardtoni.alexandru@studio.unibo.it>
2023-05-18 13:30:02 +0200

    Modified some methods in AnimationControllerImpl.

--- src/main/java/it/unibo/pixArt/controller/animation/AnimationControllerImpl.java
+++ src/main/java/it/unibo/pixArt/controller/animation/AnimationControllerImpl.java
@@ -1,11 +1,8 @@
 package it.unibo.pixArt.controller.animation;
 
-import java.util.LinkedList;
 import java.util.List;
-
 import it.unibo.pixArt.controller.SimpleController;
 import it.unibo.pixArt.model.historyframe.HistoryFrame;
-import it.unibo.pixArt.model.historyframe.HistoryFrameImpl;
 import it.unibo.pixArt.view.animation.AnimationView;
 
 
@@ -13,16 +10,8 @@ public class AnimationControllerImpl extends SimpleController implements Animati
     private Boolean isRunning = false;
     private Directions animationDirection = Directions.FORWARD;
     private int index = 0;
-    private static final String TOAD_IMAGE = "/image/toad.png";
-    private static final String SONIC_IMAGE = "/image/sonic.jpg";
-    private static final String HOMER_IMAGE = "/image/homer.jpg";
-    HistoryFrame h1 = new HistoryFrameImpl(TOAD_IMAGE);
-    HistoryFrame h2 = new HistoryFrameImpl(HOMER_IMAGE);
-    HistoryFrame h3 = new HistoryFrameImpl(SONIC_IMAGE);
-    public final List<HistoryFrame> imagePaths = new LinkedList<HistoryFrame>(List.of(h1,h2,h3));
 
     public AnimationControllerImpl() {
-        //Initialize the historyFrames list by getting each HistoryFrame from the project.
     }
 
     private class Animator extends Thread {
@@ -31,7 +20,6 @@ public class AnimationControllerImpl extends SimpleController implements Animati
             while(isRunning){
                 final HistoryFrame currentFrame = getCurrentImage();
                 getAnimationView().displayImage(currentFrame.getPath());
-        
                 try {
                     Thread.sleep(currentFrame.getAnimationDuration());
                 } catch (InterruptedException e) {
@@ -50,7 +38,6 @@ public class AnimationControllerImpl extends SimpleController implements Animati
     @Override
     public void setFrameDuration(final int frameIndex, final int duration) {
         this.getModel().getProject().getAllHistoryFrames().get(frameIndex).setAnimationDuration(duration);
-        //imagePaths.get(frameIndex).setAnimationDuration(duration);
     }
 
     @Override
@@ -80,7 +67,7 @@ public class AnimationControllerImpl extends SimpleController implements Animati
         }
         final int prev = this.index;
         this.index = this.index + animationDirection.getValue();
-        return getHistoryFrames().get(prev); //this.getModel().getProject().getAllFrames().get(prev).getHistoryFrame();
+        return getHistoryFrames().get(prev);
     }
 
     @Override
