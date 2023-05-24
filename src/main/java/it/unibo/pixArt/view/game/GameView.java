76df6de9f96f8c1e35617afbf7a849e921a8a512
Beatrice Micolucci <beatrice.micolucci@studio.unibo.it>
2023-05-24 20:44:07 +0200

    modified GameView, created game.fxml

--- src/main/java/it/unibo/pixArt/view/game/GameView.java
+++ src/main/java/it/unibo/pixArt/view/game/GameView.java
@@ -1,8 +1,16 @@
 package it.unibo.pixArt.view.game;
 
 import it.unibo.pixArt.view.AbstractFXView;
+import it.unibo.pixArt.view.pages.PageLoader;
+import it.unibo.pixArt.view.pages.Pages;
+import javafx.fxml.FXML;
 
 public class GameView extends AbstractFXView{
+
+    @FXML
+    public void onMenuClick(){
+        PageLoader.getInstance().switchPage(getStage(), Pages.MENU, this.getController().getModel());
+    }
 
     @Override
     public void init() {
