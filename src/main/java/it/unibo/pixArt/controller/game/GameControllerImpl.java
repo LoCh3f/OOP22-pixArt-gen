6ae72ee4510dbb6d2a7c799156f8ac796930f950
Beatrice Micolucci <beatrice.micolucci@studio.unibo.it>
2023-05-24 20:32:14 +0200

    created GameView, GameController and GameControllerImpl

---
+++ src/main/java/it/unibo/pixArt/controller/game/GameControllerImpl.java
@@ -0,0 +1,7 @@
+package it.unibo.pixArt.controller.game;
+
+import it.unibo.pixArt.controller.SimpleController;
+
+public class GameControllerImpl extends SimpleController implements GameController{
+    
+}
---
+++ src/main/java/it/unibo/pixArt/view/game/GameView.java
@@ -0,0 +1,13 @@
+package it.unibo.pixArt.view.game;
+
+import it.unibo.pixArt.view.AbstractFXView;
+
+public class GameView extends AbstractFXView{
+
+    @Override
+    public void init() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'init'");
+    }
+    
+}
