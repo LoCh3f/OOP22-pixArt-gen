f9f81ab91236ff9adfd2fc989ade29fb74c975cf
Eduard Toni Alexandru <eduardtoni.alexandru@studio.unibo.it>
2023-05-25 16:54:36 +0200

    Added some methods in GameSetupController and GameSetupView.

--- src/main/java/it/unibo/pixArt/controller/game/GameSetupController.java
+++ src/main/java/it/unibo/pixArt/controller/game/GameSetupController.java
@@ -1,5 +1,31 @@
 package it.unibo.pixArt.controller.game;
 
+import java.util.List;
+import java.util.Timer;
+
 public interface GameSetupController {
-    
+    /**
+     * Method to initilize the list of projects.
+     */
+    public void setProjects();
+
+    /**
+     * @return Method to get the list of projects.
+     */
+    public List<String> getProjects();
+
+    /**
+     * @return method to get all the available timers.
+     */
+    public List<String> getTimers();
+
+    /**
+     * @param time the index of timer for this game.
+     */
+    public void setTimer(final int timer);
+
+    /**
+     * @param project the index of the project for this game.
+     */
+    public void setProject(final int project);
 }
--- src/main/java/it/unibo/pixArt/controller/game/GameSetupControllerImpl.java
+++ src/main/java/it/unibo/pixArt/controller/game/GameSetupControllerImpl.java
@@ -1,7 +1,41 @@
 package it.unibo.pixArt.controller.game;
 
+import java.util.List;
+
 import it.unibo.pixArt.controller.SimpleController;
 
-public class GameSetupControllerImpl extends SimpleController implements GameSetupController{
+public class GameSetupControllerImpl extends SimpleController implements GameSetupController {
+    
+    private static String GAMES_PATH = "/games";
+
+    @Override
+    public void setProjects() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setProjects'");
+    }
+
+    @Override
+    public List<String> getProjects() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getProjects'");
+    }
+
+    @Override
+    public List<String> getTimers() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getTimers'");
+    }
+
+    @Override
+    public void setTimer(final int timer) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setTimer'");
+    }
+
+    @Override
+    public void setProject(final int project) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setProject'");
+    }
     
 }
