65d6172778aaf1d5cd232850fe38f4499687ced0
Alessandro Ricci <alessandro.ricci38@studio.unibo.it>
2023-05-29 17:04:02 +0200

    Added new method to GameControllerImplr

--- src/main/java/it/unibo/pixArt/controller/game/GameControllerImpl.java
+++ src/main/java/it/unibo/pixArt/controller/game/GameControllerImpl.java
@@ -3,8 +3,14 @@ package it.unibo.pixArt.controller.game;
 import it.unibo.pixArt.controller.SimpleController;
 import it.unibo.pixArt.model.colorstack.ColorStack;
 import it.unibo.pixArt.model.colorstack.ColorStackImpl;
+import it.unibo.pixArt.model.pixel.ImplPixel;
+import it.unibo.pixArt.model.pixel.Pixel;
+import it.unibo.pixArt.model.pixel.PixelBuilder;
 import it.unibo.pixArt.model.timer.GameTimer;
+import it.unibo.pixArt.view.game.GameView;
+import javafx.scene.image.PixelBuffer;
 import javafx.scene.paint.Color;
+import javafx.util.Pair;
 
 public class GameControllerImpl extends SimpleController implements GameController{
 
@@ -32,5 +38,14 @@ public class GameControllerImpl extends SimpleController implements GameControll
 
     @Override
     public void checkPixel(int x, int y, Color color) {
+        var pixel = new PixelBuilder.PxlBuilder().setColor(color).setX(x).setY(y).build();
+        if(this.colorStack.getColorMap().get(color).stream().anyMatch(p -> p.comparePixel(pixel))){
+            this.getGameView().colorButton(x, y);
+            this.colorStack.removePixel(color, pixel);
+        }
+    }
+
+    private GameView getGameView(){
+        return (GameView) this.getView();
     }
 }
