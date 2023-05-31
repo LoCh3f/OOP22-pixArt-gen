d05942de4716e89e7ebf43247b24e97053fade37
Alessandro Ricci <alessandro.ricci38@studio.unibo.it>
2023-05-31 11:03:00 +0200

    Added javadoc in GameController

--- src/main/java/it/unibo/pixArt/controller/game/GameController.java
+++ src/main/java/it/unibo/pixArt/controller/game/GameController.java
@@ -10,17 +10,41 @@ import javafx.scene.paint.Color;
 
 public interface GameController extends Controller{
 
+    /**
+     * @return The timer of the game
+     */
     GameTimer getTimer();
 
+    /**
+     * @return The size of the frame
+     */
     int getFrameSize();
 
+    /**
+     * @return The map of the color stack 
+     */
     Map<Color,Set<Pixel>> getColorStack();
 
+    /**
+     * Set the color stack to be used
+     */
     void setColorStack();
     
+    /**
+     * @param x The x coordinate of the pixel
+     * @param y The y coordinates of the pixel
+     * @param color The color selected
+     * @return True if the pixel int position x, y has the same color of the corresponding pixel in the color stack
+     */
     boolean checkPixel(final int x, final int y, final Color color);
 
+    /**
+     * @return True if the color stack is empty
+     */
     boolean colorStackIsEmpty();
 
+    /**
+     * @return The percentage of the pixel left withing the map
+     */
     float getPercentage();
 }
