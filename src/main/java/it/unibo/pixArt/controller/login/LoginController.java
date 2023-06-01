dc86057f482bd0d6a203a3560350d98b8279a464
Beatrice Micolucci <beatrice.micolucci@studio.unibo.it>
2023-06-01 15:42:07 +0200

    solved checkstyle in login and timer classes

--- src/main/java/it/unibo/pixArt/controller/login/LoginController.java
+++ src/main/java/it/unibo/pixArt/controller/login/LoginController.java
@@ -5,15 +5,15 @@ import java.io.IOException;
 import it.unibo.pixArt.controller.Controller;
 import it.unibo.pixArt.model.user.validator.ValidationResult;
 
-public interface LoginController extends Controller{
-    
+public interface LoginController extends Controller {
+
     /**
      * @param username
      * @param password
      * @return true if the user is logged, false otherwise
      * @throws IOException
      */
-    public boolean login(final String username, final String password) throws IOException;
+    boolean login(String username, String password) throws IOException;
 
     /**
      * @param username
@@ -22,29 +22,29 @@ public interface LoginController extends Controller{
      * @return true if the user can be registered, false otherwise
      * @throws IOException
      */
-    public boolean register(final String username, final String password, final String path) throws IOException;
+    boolean register(String username, String password, String path) throws IOException;
 
     /**
      * @param path
      */
-    public void guestLogin(final String path);
+    void guestLogin(String path);
 
     /**
      * @param username
      * @return the result of the username validation
      */
-    public ValidationResult usernameValidation(final String username);
+    ValidationResult usernameValidation(String username);
 
     /**
      * @param password
      * @return the result of the password validation
      */
-    public ValidationResult passwordValidation(final String password);
+    ValidationResult passwordValidation(String password);
 
     /**
      * @param path
      * @return the result of the path validation
      */
-    public ValidationResult pathValidation(final String path);
+    ValidationResult pathValidation(String path);
 
 }
