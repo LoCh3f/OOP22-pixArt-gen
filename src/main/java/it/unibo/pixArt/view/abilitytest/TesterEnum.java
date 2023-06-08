88a4361799d030588fc2d927371d55b300d2093a
Aldo Visconti <aldovisco20000@gmail.com>
2023-06-08 15:56:04 +0200

    checkstyle in package view/abilitytest

--- src/main/java/it/unibo/pixArt/view/abilitytest/TesterEnum.java
+++ src/main/java/it/unibo/pixArt/view/abilitytest/TesterEnum.java
@@ -6,17 +6,17 @@ public enum TesterEnum {
 
     VERY_GOOD(0, IMAGE_PATH + IMAGE_VERY_GOOD),
 
-    GOOD(40,IMAGE_PATH + IMAGE_GOOD),
+    GOOD(40, IMAGE_PATH + IMAGE_GOOD),
 
-    TRY_BETTER(80,IMAGE_PATH + IMAGE_TRY_BETTER),
+    TRY_BETTER(80, IMAGE_PATH + IMAGE_TRY_BETTER),
 
-    BAD(120,IMAGE_PATH + IMAGE_BAD),
+    BAD(120, IMAGE_PATH + IMAGE_BAD),
 
-    VERY_BAD(160,IMAGE_PATH + IMAGE_VERY_BAD),
+    VERY_BAD(160, IMAGE_PATH + IMAGE_VERY_BAD),
 
-    TOO_MUCH(200,IMAGE_PATH + IMAGE_TOO_MUCH),
+    TOO_MUCH(200, IMAGE_PATH + IMAGE_TOO_MUCH),
 
-    WASTED(256,IMAGE_PATH + IMAGE_WASTED);
+    WASTED(256, IMAGE_PATH + IMAGE_WASTED);
 
     private final String path;
 
@@ -28,10 +28,16 @@ public enum TesterEnum {
         this.path = path;
     }
 
+    /**
+     * @return the path of the image
+     */
     public String getPath() {
-        return  this.path;
+        return this.path;
     }
 
+    /**
+     * @return the limit of the difference
+     */
     public int getLimit() {
         return this.limit;
     }
