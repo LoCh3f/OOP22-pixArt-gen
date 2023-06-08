7ea814cd97d96a65419a30ceec9a7f6b9025b8f5
Eduard Toni Alexandru <eduardtoni.alexandru@studio.unibo.it>
2023-06-08 22:11:06 +0200

    Solved GameLevels checkStyles.

--- src/main/java/it/unibo/pixArt/model/game/GameLevels.java
+++ src/main/java/it/unibo/pixArt/model/game/GameLevels.java
@@ -1,18 +1,17 @@
 package it.unibo.pixArt.model.game;
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
-import java.nio.file.CopyOption;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-
+/**
+ * Enum for all the available game levels.
+ */
 public enum GameLevels {
 
     SKULL("/games/Skull/Skull.json", "/games/Skull/Skull0.png"),
@@ -38,11 +37,11 @@ public enum GameLevels {
         return Stream.of(values()).collect(Collectors.toList());
     }
 
-    private String fileInput() throws IOException{
+    private String fileInput() throws IOException {
         InputStream inputStream = this.getClass().getResourceAsStream(this.pathToFile);
         new File(System.getProperty("user.home") + File.separatorChar + "PixArtDatas").mkdir();
         File tempFile = new File(System.getProperty("user.home") + "/PixArtDatas/tempGame.json");
-        
+
         Files.copy(inputStream, tempFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
 
         inputStream.close();
