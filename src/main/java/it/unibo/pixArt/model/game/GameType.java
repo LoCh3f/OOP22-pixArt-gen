bf5e44c1bb61929fbda95e245dc81929d825f062
Eduard Toni Alexandru <eduardtoni.alexandru@studio.unibo.it>
2023-06-08 22:15:46 +0200

    Solved GameType checkStyles.

--- src/main/java/it/unibo/pixArt/model/game/GameType.java
+++ src/main/java/it/unibo/pixArt/model/game/GameType.java
@@ -4,18 +4,20 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+/**
+ * Enum containing all the available game modes.
+ */
 public enum GameType {
-    COLORBOOK("Color Book", "A game in which there is a number associated to every available color," + 
-                        " and the user has to color each button with the correct color." + 
-                        "Every button contains the associated number. The user has to finish before the time ends,"+ 
-                        " and gets a score based on the number of the buttons colored correctly."),
+    COLORBOOK("Color Book", "A game in which there is a number associated to every available color,"
+            + " and the user has to color each button with the correct color."
+            + "Every button contains the associated number. The user has to finish before the time ends,"
+            +  " and gets a score based on the number of the buttons colored correctly."),
+    MIRROR("Mirror", "A game in which the user has to replicate an image displayed on the screen, before the timer ends."
+            + "The user gets a score based on how similar the grid is to the immage.");
 
-    MIRROR("Mirror", "A game in which the user has to replicate an image displayed on the screen, before the timer ends."+
-                        "The user gets a score based on how similar the grid is to the immage.");
-    
     private String name;
     private String description;
-    GameType(final String name, final String description){
+    GameType(final String name, final String description) {
         this.name = name;
         this.description = description;
     }
@@ -24,11 +26,11 @@ public enum GameType {
         return this.name;
     }
 
-    public String getDescription(){
+    public String getDescription() {
         return this.description;
     }
 
-    public static List<GameType> getGameTypes(){
+    public static List<GameType> getGameTypes() {
         return Stream.of(values()).collect(Collectors.toList());
     }
 }
