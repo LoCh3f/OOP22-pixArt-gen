095305ce90cdfaf5d7febea6d052fd8a3f8d9808
Eduard Toni Alexandru <eduardtoni.alexandru@studio.unibo.it>
2023-06-08 17:42:40 +0200

    Solved HomeView checkstyles.

--- src/main/java/it/unibo/pixArt/view/impl/HomeView.java
+++ src/main/java/it/unibo/pixArt/view/impl/HomeView.java
@@ -47,22 +47,22 @@ public final class HomeView extends AbstractFXView {
 
     @FXML
     public void onBackClick(final ActionEvent event) {
-        SceneManager.getInstance().switchPage(this.getStage(), Pages.LOGIN, new ModelImpl(null, null, null, null , null));
+        SceneManager.getInstance().switchPage(this.getStage(), Pages.LOGIN, new ModelImpl(null, null, null, null, null));
     }
 
     @Override
     public void init() {
 
-      AnchorPane.setLeftAnchor(this.imageView,0d);
+      AnchorPane.setLeftAnchor(this.imageView, 0d);
       this.imageView.fitWidthProperty().bind(this.getStage().widthProperty().divide(2));
       this.imageView.fitHeightProperty().bind(this.getStage().heightProperty());
 
-      AnchorPane.setRightAnchor(this.buttonList,0d);
+      AnchorPane.setRightAnchor(this.buttonList, 0d);
       this.buttonList.setPadding(new Insets(10));
       this.buttonList.setAlignment(Pos.TOP_RIGHT);
       this.buttonList.prefWidthProperty().bind(this.getStage().widthProperty().divide(2).subtract(10));
       this.buttonList.prefHeightProperty().bind(this.getStage().heightProperty());
-      this.buttonList.getChildren().forEach( c -> {
+      this.buttonList.getChildren().forEach(c -> {
           final var b = (Button) c;
           b.prefWidthProperty().bind(this.buttonList.widthProperty().subtract(20d));
           b.maxHeight(Double.MAX_VALUE);
