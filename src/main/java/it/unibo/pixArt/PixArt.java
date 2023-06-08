5c1f518dc997fce65e2aa324f226ba31f2e62a18
Eduard Toni Alexandru <eduardtoni.alexandru@studio.unibo.it>
2023-06-08 18:17:20 +0200

    Solved PixArt checkstyles.

--- src/main/java/it/unibo/pixArt/PixArt.java
+++ src/main/java/it/unibo/pixArt/PixArt.java
@@ -1,7 +1,7 @@
 package it.unibo.pixArt;
 
 public class PixArt {
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         App.main(args);
     }
 }
