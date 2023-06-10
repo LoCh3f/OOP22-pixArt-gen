6b72fed66e430fa56eeecb49fa7d5fd6a9716d64
Aldo Visconti <aldovisco20000@gmail.com>
2023-06-10 15:43:00 +0200

    checkstyle for stage distribution

--- src/main/java/it/unibo/pixart/view/components/StageDistribution.java
+++ src/main/java/it/unibo/pixart/view/components/StageDistribution.java
@@ -1,17 +1,39 @@
 package it.unibo.pixart.view.components;
 
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
+import java.util.List;
+import java.util.Set;
+
+/**
+ * This interface is still in a primitive state.
+ * I would like to use it to create multiple stages in a fastest way at the same time.
+ */
 public interface StageDistribution {
 
-    public final Double PREF_WIDTH = 900.0;
-    public final Double PREF_HEIGHT = 700.0;
+    /**
+     * @param roots  the root of the stage;
+     * @param titles the titles of the stages;
+     * @param icons  the icons of the stages;
+     * @param nodes  the nodes of the stages;
+     * @return the set of stages.
+     */
+    Set<Stage> dropStages(List<Parent> roots, List<String> titles, List<Image> icons, List<Node> nodes);
 
+    /**
+     * Class that extends Stage for build more complex in a fastest way.
+     */
     class ParallelStage extends Stage {
 
+        /**
+         * @param root  the root of the stage;
+         * @param title the title of the stage;
+         * @param icon  the icon of the stage;
+         */
         public ParallelStage(final Parent root, final String title, final Image icon) {
             super();
             setTitle(title);
