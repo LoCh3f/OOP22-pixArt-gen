8598d05158e6c88a80e9f2050907788c21660202
Aldo Visconti <aldovisco20000@gmail.com>
2023-06-10 14:29:56 +0200

    checkstyle for package utilities/variables

--- src/main/java/it/unibo/pixart/utilities/variables/FXViewVariables.java
+++ src/main/java/it/unibo/pixart/utilities/variables/FXViewVariables.java
@@ -1,31 +1,73 @@
 package it.unibo.pixart.utilities.variables;
 
+/**
+ * Classe che contiene le variabili per la gestione dello stile della view
+ */
 public class FXViewVariables {
 
+    /**
+     * String for set style of the border
+     */
     public static final String FX_BORDER_COLOR = "-fx-border-color:black";
 
+    /**
+     * String for set style of the background
+     */
     public static final String FX_BACKGROUND_COLOR_START = "-fx-background-color:";
 
+    /**
+     * String for set style of the border width
+     */
     public static final String FX_BORDER_WIDTH = "-fx-border-width:1";
 
+    /**
+     * String for the image path start
+     */
     public static final String IMAGE_PATH = "image/";
 
+    /**
+     * String for the image path
+     */
     public static final String IMAGE_BAD = "bad.jpg";
 
+    /**
+     * String for the image path
+     */
     public static final String IMAGE_VERY_BAD = "veryBad.jpg";
 
+    /**
+     * String for the image path
+     */
     public static final String IMAGE_GOOD = "good.jpg";
 
+    /**
+     * String for the image path
+     */
     public static final String IMAGE_VERY_GOOD = "veryGood.jpg";
 
+    /**
+     * String for the image path
+     */
     public static final String MAIN_ICON = "mainIcon.png";
 
+    /**
+     * String for the image path
+     */
     public static final String MAIN_LOGO = "mainLogo.png";
 
+    /**
+     * String for the image path
+     */
     public static final String IMAGE_TOO_MUCH = "tooMuchBad.jpg";
 
+    /**
+     * String for the image path
+     */
     public static final String IMAGE_TRY_BETTER = "tryBetter.jpg";
 
+    /**
+     * String for the image path
+     */
     public static final String IMAGE_WASTED = "wasted.jpg";
 
 
