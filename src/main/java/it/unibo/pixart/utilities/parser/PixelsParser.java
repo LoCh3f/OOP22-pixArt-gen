1261510f532a86651b6f67b8dd81cdd677b466a4
Aldo Visconti <aldovisco20000@gmail.com>
2023-06-10 15:19:57 +0200

    checkstyle for PixelsParser.java

--- src/main/java/it/unibo/pixart/utilities/parser/PixelsParser.java
+++ src/main/java/it/unibo/pixart/utilities/parser/PixelsParser.java
@@ -1,21 +1,24 @@
 package it.unibo.pixart.utilities.parser;
 
+import it.unibo.pixart.model.pixel.Pixel;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 
-import static it.unibo.pixart.utilities.variables.FXViewVariables.*;
-
 import java.util.Collection;
 import java.util.function.Function;
 
-import it.unibo.pixart.model.pixel.Pixel;
+import static it.unibo.pixart.utilities.variables.FXViewVariables.*;
 
 /**
  * This class is used to convert a Collection<Pixel>,
  * in a gridPane that represent the Collection in input.
  */
-public class PixelsParser implements Function<Collection<Pixel>, GridPane> {
+public final class PixelsParser implements Function<Collection<Pixel>, GridPane> {
+    /**
+     * @param pixels the function argument (Collection<Pixel>)
+     * @return a GridPane that represent the Collection in input
+     */
     @Override
     public GridPane apply(final Collection<Pixel> pixels) {
         final var gridPane = new GridPane();
@@ -26,7 +29,14 @@ public class PixelsParser implements Function<Collection<Pixel>, GridPane> {
         return gridPane;
     }
 
+    /**
+     * @param color the color to parse
+     * @return a String that represent the color in input for set the style of the button
+     */
     public String parseColor(final Color color) {
-        return "".concat(FX_BACKGROUND_COLOR_START).concat(color.toString().replace("0x", "#")).concat(";").concat(FX_BORDER_COLOR).concat(";").concat(FX_BORDER_WIDTH);
+        return "".concat(FX_BACKGROUND_COLOR_START)
+                .concat(color.toString().replace("0x", "#"))
+                .concat(";").concat(FX_BORDER_COLOR).concat(";")
+                .concat(FX_BORDER_WIDTH);
     }
 }
