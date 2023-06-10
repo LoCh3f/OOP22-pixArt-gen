d8a096888d309e17ded1866e45562cf5d9fb06f0
Aldo Visconti <aldovisco20000@gmail.com>
2023-06-10 15:00:02 +0200

    Âcheckstyle in package utilities/mirror

--- src/main/java/it/unibo/pixart/utilities/mirror/GenericMirror.java
+++ src/main/java/it/unibo/pixart/utilities/mirror/GenericMirror.java
@@ -4,9 +4,12 @@ import java.util.Collection;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-/*
+/**
+ * @param <X> the type of the collection.
+ * @see Mirror
+ * /*
  * This is a simple implementation pf interface mirror,
- * the generic type choose  must override method equals from s.c. Object to work in this class;
+ * the generic type chosen  must override method equals from s.c. Object to work in this class;
  */
 public class GenericMirror<X> implements Mirror<X> {
 
