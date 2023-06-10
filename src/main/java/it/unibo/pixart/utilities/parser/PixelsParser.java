2ddb84d26931573232022000b1c4e63f56537ea2
Eduard Toni Alexandru <eduardtoni.alexandru@studio.unibo.it>
2023-06-10 19:57:44 +0200

    Solved checkstyles .

--- src/main/java/it/unibo/pixart/utilities/parser/PixelsParser.java
+++ src/main/java/it/unibo/pixart/utilities/parser/PixelsParser.java
@@ -8,7 +8,9 @@ import javafx.scene.paint.Color;
 import java.util.Collection;
 import java.util.function.Function;
 
-import static it.unibo.pixart.utilities.variables.FXViewVariables.*;
+import static it.unibo.pixart.utilities.variables.FXViewVariables.FX_BACKGROUND_COLOR_START;
+import static it.unibo.pixart.utilities.variables.FXViewVariables.FX_BORDER_COLOR;
+import static it.unibo.pixart.utilities.variables.FXViewVariables.FX_BORDER_WIDTH;;
 
 /**
  * This class is used to convert a Collection<Pixel>,
